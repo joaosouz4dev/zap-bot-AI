48149d599577e30919fe668313527c77aa206526
João Victor Souza <souza.joao1997@gmail.com>
2020-11-09 14:07:15 -0300

    update

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const venom = require('venom-bot');
 const dialogflow = require('@google-cloud/dialogflow');
 
 // INICIANDO O DIALOGFLOW E CONFIGURANDO COM O USO DA KEY GERADA EM JSON, DEVE SE ENCONTRAR NA RAIZ DO PROJETO
-const sessionClient = new dialogflow.SessionsClient({keyFilename: "./teste-qrscuw-872f2225e85c.json"});
+const sessionClient = new dialogflow.SessionsClient({keyFilename: "</YOURPATHJSON>"});
+
 // FUNCOES USADAS DA DOCUMENTACAO NORMAL QUE ESTAO NO README.md EM LINKS ÚTEIS
 async function detectIntent(
     projectId,
@@ -62,17 +63,7 @@ async function executeQueries(projectId, sessionId, queries, languageCode) {
 
 // INICIANDO O VENOM BOT
 // PARAMETROS USADO PELO VENOM
-const parameters = {
-    headless: true, // Headless chrome
-    devtools: false, // Open devtools by default
-    useChrome: true, // If false will use Chromium instance
-    debug: false, // Opens a debug session
-    logQR: true, // Logs QR automatically in terminal
-    browserArgs: [''], // Parameters to be added into the chrome browser instance
-    refreshQR: 15000, // Will refresh QR every 15 seconds, 0 will load QR once. Default is 30 seconds
-    autoClose: 60000, // Will auto close automatically if not synced, 'false' won't auto close. Default is 60 seconds (#Important!!! Will automatically set 'refreshQR' to 1000#)
-    disableSpins: true, // Will disable Spinnies animation, useful for containers (docker) for a better log
-}
+
 // INICIANDO CREATE DO VENOM
 venom.create('sessionName').then((client) => start(client));
 // FUNCAO START DO CLIENTE DO VENOM BOT
@@ -80,7 +71,7 @@ function start(client) {
     // FUNCAO QUE CAPTURA AS MENSAGEM CHEGADAS DO WPP USANDO O VENOM
     client.onMessage(async message => {
         // EXECUTANDO QUERY DIALOGFLOW
-        let textoResposta = await executeQueries("teste-qrscuw", message.from, [message.body], 'pt-BR')
+        let textoResposta = await executeQueries("guia-obmn", message.from, [message.body], 'pt-BR')
         // ENVIANDO QUERY RETORNADA
         await client.sendText(message.from, textoResposta);
     })
